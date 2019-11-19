48acc0be0d654ca414c6fa4df8a55a7f2c7ea50d
tolauren19 <lauren_to@brown.edu>
2019-11-19 15:31:55 +0000

    etl.py

--- etl.py
+++ etl.py
@@ -26,7 +26,7 @@ def load_airbnb(file):
         d.pop('longitude')
         d.pop('latitude')
         arr.append(d.copy())
-    print("Airbnb document fileds: ", arr[0].keys())
+    print("Airbnb document fields: ", arr[0].keys())
 
     inserted_ids = db.airbnb.insert_many(arr).inserted_ids
     db.airbnb.create_index(
@@ -52,6 +52,32 @@ def load_taxi(file):
 
     # TODO: insert `arr` into `db.taxi` and print the number of records inserted.
     # Use load_airbnb as an example. This takes 2 lines of codes.
+    arr = []
+    df = pds.read_csv(file)
+    for i, row in df.iterrows():
+        d = row.to_dict()
+        d['pickup'] = {
+            'type': 'Point',
+            'coordinates': [
+                d['pickup_longitude'], d['pickup_latitude']
+            ]}
+        d['dropoff'] = {
+            'type': 'Point',
+            'coordinates': [
+                d['dropoff_longitude'], d['dropoff_latitude']
+            ]}
+        d.pop('pickup_longitude')
+        d.pop('pickup_latitude')
+        d.pop('dropoff_longitude')
+        d.pop('dropoff_latitude')
+        arr.append(d.copy())
+    print("Taxi document fields: ", arr[0].keys())
+
+    inserted_ids = db.taxi.insert_many(arr).inserted_ids
+    # db.taxi.create_index(
+    #     [('name', TEXT), ('neighbourhood', TEXT)], default_language='english')
+    db.taxi.create_index([('pickup', GEOSPHERE), ('dropoff', GEOSPHERE)])
+    print(len(inserted_ids), "taxi documents inserted")
 
 
 if __name__ == "__main__":
